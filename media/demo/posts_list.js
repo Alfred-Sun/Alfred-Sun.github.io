1928a082b8a87bb24681d5c724f196f6185abbdc
Alfred Sun <autogappproxy@gmail.com>
2017-03-12 23:01:54 +0800

    delete source posts and upload generated html files

--- media/demo/posts_list.js
+++ media/demo/posts_list.js
@@ -1,12 +1,44 @@
----
-layout: null
----
-
 var _POSTS = {
-	"list": [{% for post in site.posts limit: 10 %}{
-		"title": "{{post.title}}",
-		"url": "{{site.url}}{{post.url}}",
-		"date": "{{post.date|date_to_string}}"
-		}{% if forloop.last == false %},{% endif %}{% endfor %}
+	"list": [{
+		"title": "Linux 守护进程的实现",
+		"url": "http://alfred-sun.github.io/blog/2015/06/18/daemon-implementation/",
+		"date": "18 Jun 2015"
+		},{
+		"title": "Bash 学习笔记",
+		"url": "http://alfred-sun.github.io/blog/2015/05/13/bash-notes/",
+		"date": "13 May 2015"
+		},{
+		"title": "启用 Xfce 桌面的事件声音",
+		"url": "http://alfred-sun.github.io/blog/2015/05/06/how-to-enable-event-sounds-in-xfce/",
+		"date": "06 May 2015"
+		},{
+		"title": "开源编辑器：Visual Studio Code",
+		"url": "http://alfred-sun.github.io/blog/2015/05/05/visual-studio-code/",
+		"date": "05 May 2015"
+		},{
+		"title": "如何启用 root 用户登录 Fedora ？",
+		"url": "http://alfred-sun.github.io/blog/2015/05/05/problems-with-using-chrome/",
+		"date": "05 May 2015"
+		},{
+		"title": "学习 tar 压缩命令",
+		"url": "http://alfred-sun.github.io/blog/2015/05/04/linux-tar-command/",
+		"date": "04 May 2015"
+		},{
+		"title": "如何启用 root 用户登录 Fedora ？",
+		"url": "http://alfred-sun.github.io/blog/2015/05/03/install-vmware-tools-on-linux/",
+		"date": "03 May 2015"
+		},{
+		"title": "如何启用 root 用户登录 Fedora ？",
+		"url": "http://alfred-sun.github.io/blog/2015/05/01/install-openbox-wm/",
+		"date": "01 May 2015"
+		},{
+		"title": "如何启用 root 用户登录 Fedora ？",
+		"url": "http://alfred-sun.github.io/blog/2015/04/29/login-fedora-with-root/",
+		"date": "29 Apr 2015"
+		},{
+		"title": "十个基础算法及其讲解",
+		"url": "http://alfred-sun.github.io/blog/2015/03/11/ten-basic-algorithms-for-programmers/",
+		"date": "11 Mar 2015"
+		}
 	]
 };
