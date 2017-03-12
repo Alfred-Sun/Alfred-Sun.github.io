703dbef09173c29aa668d70d8b542973e569f3bc
Alfred Sun <autogappproxy@gmail.com>
2017-03-13 00:28:28 +0800

    update posts

--- media/demo/posts_list.js
+++ media/demo/posts_list.js
@@ -16,21 +16,9 @@ var _POSTS = {
 		"url": "http://alfred-sun.github.io/blog/2015/05/05/visual-studio-code/",
 		"date": "05 May 2015"
 		},{
-		"title": "如何启用 root 用户登录 Fedora ？",
-		"url": "http://alfred-sun.github.io/blog/2015/05/05/problems-with-using-chrome/",
-		"date": "05 May 2015"
-		},{
 		"title": "学习 tar 压缩命令",
 		"url": "http://alfred-sun.github.io/blog/2015/05/04/linux-tar-command/",
 		"date": "04 May 2015"
-		},{
-		"title": "如何启用 root 用户登录 Fedora ？",
-		"url": "http://alfred-sun.github.io/blog/2015/05/03/install-vmware-tools-on-linux/",
-		"date": "03 May 2015"
-		},{
-		"title": "如何启用 root 用户登录 Fedora ？",
-		"url": "http://alfred-sun.github.io/blog/2015/05/01/install-openbox-wm/",
-		"date": "01 May 2015"
 		},{
 		"title": "如何启用 root 用户登录 Fedora ？",
 		"url": "http://alfred-sun.github.io/blog/2015/04/29/login-fedora-with-root/",
@@ -39,6 +27,18 @@ var _POSTS = {
 		"title": "十个基础算法及其讲解",
 		"url": "http://alfred-sun.github.io/blog/2015/03/11/ten-basic-algorithms-for-programmers/",
 		"date": "11 Mar 2015"
+		},{
+		"title": "Bash 快捷键",
+		"url": "http://alfred-sun.github.io/blog/2015/03/08/bash-special-key/",
+		"date": "08 Mar 2015"
+		},{
+		"title": "Vim 常用命令小结",
+		"url": "http://alfred-sun.github.io/blog/2015/03/04/vim-commands-frequently-used/",
+		"date": "04 Mar 2015"
+		},{
+		"title": "Vim技巧——将 CapsLock 键映射成 Esc键",
+		"url": "http://alfred-sun.github.io/blog/2015/02/04/remap-capslock-key/",
+		"date": "04 Feb 2015"
 		}
 	]
 };
